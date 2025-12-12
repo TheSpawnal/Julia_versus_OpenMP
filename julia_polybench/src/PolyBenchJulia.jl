002be0b6db2d4af54e58db59a7b1bccad635bedb
ALJDBS <70330110+TheSpawnal@users.noreply.github.com>
2025-12-12 16:04:20 +0100

    Create PolyBenchJulia.jl

---
+++ julia_polybench/src/PolyBenchJulia.jl
@@ -0,0 +1,36 @@
+module PolyBenchJulia
+
+# Common utilities
+include("common/Config.jl")
+include("common/Metrics.jl")
+include("common/BenchCore.jl")
+
+# Kernel implementations
+include("kernels/Cholesky.jl")
+include("kernels/Correlation.jl")
+include("kernels/Jacobi2D.jl")
+
+# Re-export modules
+using .Config
+using .Metrics
+using .BenchCore
+using .Cholesky
+using .Correlation
+using .Jacobi2D
+
+export Config, Metrics, BenchCore
+export Cholesky, Correlation, Jacobi2D
+
+# Export common functions
+export configure_blas_threads, print_system_info
+export MetricsCollector, BenchmarkResult, record!, print_results, export_csv
+export benchmark_kernel, TimingResult
+
+# Version
+const VERSION = v"1.0.0"
+
+function __init__()
+    configure_blas_threads()
+end
+
+end # module
